4abb9c804f9ba6ca6f27cf12f8f6ff83ca2280b5
Kcheng199 <onepupuman@gmail.com>
2026-03-29 18:26:36 +1100

    Initial commit - created Django quiz project

---
+++ quiz/views.py
@@ -0,0 +1,14 @@
+import random
+from django.shortcuts import render
+
+def quiz_view(request):
+
+    num1 = random.randint(1,10)
+    num2 = random.randint(1,10)
+
+    context = {
+        "num1": num1,
+        "num2": num2
+    }
+
+    return render(request, "quiz.html", context)
