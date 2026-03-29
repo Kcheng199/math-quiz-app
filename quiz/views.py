fdb49cb60d83d12d29a6b8edb4cc0ec5fa50fdaa
Kcheng199 <onepupuman@gmail.com>
2026-03-30 10:34:16 +1100

    Fixed template location and project structure

--- quiz/views.py
+++ quiz/views.py
@@ -4,13 +4,15 @@ import random
 from django.shortcuts import render
 
 def quiz_view(request):
+    
+    # generate two random numbers
+    num1 = random.randint(1, 10)
+    num2 = random.randint(1, 10)
 
-    num1 = random.randint(1,10)
-    num2 = random.randint(1,10)
-
+    # send data to HTML
     context = {
-        "num1": num1,
-        "num2": num2
+        'num1': num1,
+        'num2': num2
     }
 
-    return render(request, "quiz.html", context)
+    return render(request, 'quiz.html', context)
